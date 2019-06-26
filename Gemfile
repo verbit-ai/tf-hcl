c8064745ff81ec5afce95ac5d89618d9f802b3a5
Roman Yarovoy <roman@yarovoy.me>
2019-06-26 05:51:08 +0300

    Remove broken submodules & update deps.

--- Gemfile
+++ Gemfile
@@ -1,6 +1,4 @@
 source "https://rubygems.org"
 
-git_source(:github) {|repo_name| "https://github.datapipe.net/Automation/#{repo_name}" }
-
 # Specify your gem's dependencies in tf-hcl.gemspec
 gemspec
--- clone_tf_sample_repos.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-git clone https://github.com/terraform-aws-modules/terraform-aws-vpc spec/sample_data/terraform-aws-vpc
-git clone https://github.com/terraform-aws-modules/terraform-aws-alb spec/sample_data/terraform-aws-alb
